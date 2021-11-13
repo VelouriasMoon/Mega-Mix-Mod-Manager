6bb9e0f6ea0424af06329b15e6bfac66d54936b0
Moonling <52844176+VelouriasMoon@users.noreply.github.com>
2021-11-13 17:06:46 -0400

    Create StreamReaderLookAhead.cs

---
+++ Mega Mix Mod Manager/IO/StreamReaderLookAhead.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Mega_Mix_Mod_Manager.IO
+{
+    public static class StreamReaderLookAhead
+    {
+        public static string LookAheadLine(StreamReader sr)
+        {
+            long pos = sr.BaseStream.Position;
+            string line = sr.ReadLine();
+
+            if (line == null)
+                return null;
+
+            sr.BaseStream.Seek(pos, SeekOrigin.Begin);
+            return line;
+        }
+    }
+}
