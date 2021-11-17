c90c4571eab84c7f8d50343000a9507c8488e4b4
Moonling <52844176+VelouriasMoon@users.noreply.github.com>
2021-11-17 02:23:50 -0400

    started farc merger

---
+++ Mega Mix Mod Manager/Lite Merge/farc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MikuMikuLibrary.Archives;
+using MikuMikuLibrary.IO.Common;
+
+namespace Mega_Mix_Mod_Manager.Lite_Merge
+{
+    internal class farc
+    {
+        private static FarcArchive Read(string infile)
+        {
+            using (FileStream fs = new FileStream(infile, FileMode.Open))
+            {
+                using (EndianBinaryReader ebr = new EndianBinaryReader(fs, MikuMikuLibrary.IO.Endianness.Little))
+                {
+                    FarcArchive farcArchive = new FarcArchive();
+                    farcArchive.Read(ebr);
+                    return farcArchive;
+                }
+            }
+        }
+    }
+}
