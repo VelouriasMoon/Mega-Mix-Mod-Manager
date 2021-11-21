2678a390002ccc0873a2eb96ec9db0c6c5eb208e
Moonling <52844176+VelouriasMoon@users.noreply.github.com>
2021-11-21 05:03:58 -0400

    sprite property write function

--- Mega Mix Mod Manager/Editors/Database/Property_Spr.cs
+++ Mega Mix Mod Manager/Editors/Database/Property_Spr.cs
@@ -53,5 +53,26 @@ namespace Mega_Mix_Mod_Manager.Editors.Database
                 Textures.Add(databaseObject);
             }
         }
+
+        public SpriteSetInfo Write()
+        {
+            SpriteSetInfo spriteSetInfo = new SpriteSetInfo();
+            spriteSetInfo.Name = Name;
+            spriteSetInfo.Id = ID;
+            spriteSetInfo.FileName = FileName;
+
+            foreach (DatabaseObject spr in Sprites)
+            {
+                SpriteInfo spriteInfo = new SpriteInfo() { Name = spr.Name, Id = spr.ID, Index = (ushort)spr.Index};
+                spriteSetInfo.Sprites.Add(spriteInfo);
+            }
+            foreach (DatabaseObject tex in Textures)
+            {
+                SpriteTextureInfo spriteTextureInfo = new SpriteTextureInfo() { Name = tex.Name, Id = tex.ID, Index = (ushort)tex.Index};
+                spriteSetInfo.Textures.Add(spriteTextureInfo);
+            }
+
+            return spriteSetInfo;
+        }
     }
 }
