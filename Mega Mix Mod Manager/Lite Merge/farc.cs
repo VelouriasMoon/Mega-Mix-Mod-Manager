c00e012b111dc1ce0eba878f5bfceb03f2055b84
Moonling <52844176+VelouriasMoon@users.noreply.github.com>
2021-11-21 02:53:07 -0400

    Wip farc merger

--- Mega Mix Mod Manager/Lite Merge/farc.cs
+++ Mega Mix Mod Manager/Lite Merge/farc.cs
@@ -5,13 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MikuMikuLibrary.Archives;
+using MikuMikuLibrary.IO;
 using MikuMikuLibrary.IO.Common;
 
 namespace Mega_Mix_Mod_Manager.Lite_Merge
 {
     internal class farc
     {
-        public static FarcArchive Read(string infile)
+        private static FarcArchive Read(string infile)
         {
             using (FileStream fs = new FileStream(infile, FileMode.Open))
             {
@@ -21,6 +22,52 @@ namespace Mega_Mix_Mod_Manager.Lite_Merge
                     farcArchive.Read(ebr);
                     return farcArchive;
                 }
+            }
+        }
+
+        public static void Merge(string dumpPath, string ModPath, string outPath)
+        {
+            string[] modFiles = Directory.GetFiles(ModPath, "*.farc", SearchOption.AllDirectories);
+            string[] vanillaFiles = Directory.GetFiles(dumpPath, "*.farc", SearchOption.AllDirectories);
+
+            //file loop to check and merge each file in the mods folder
+            foreach (string file in modFiles)
+            {
+                MemoryStream ms = new MemoryStream();
+                FarcArchive Final = new FarcArchive();
+                //check if the game dump contains the modded file
+                if (vanillaFiles.Contains( vanillaFiles.LastOrDefault(x => x.Contains(file.Substring(file.LastIndexOf("rom")))) ))
+                {
+                    string VanillaFile = vanillaFiles.LastOrDefault(x => x.Contains(file.Substring(file.LastIndexOf("rom"))));
+                    if (VanillaFile.Contains("2d"))
+                        continue;
+
+                    FileStream fs = File.OpenRead(VanillaFile);
+                    Final = BinaryFile.Load<FarcArchive>(fs);
+
+                    using (var Stream = File.OpenRead(file))
+                    {
+                        FarcArchive ModFarc = BinaryFile.Load<FarcArchive>(Stream);
+
+                        //loop through the mod file's contents to find what to add to vanilla farc
+                        foreach (string entry in ModFarc)
+                        {
+                            using (var source = ModFarc.Open(entry, EntryStreamMode.MemoryStream))
+                            {
+                                Final.Add(entry, source, false, ConflictPolicy.Replace);
+                            }
+
+                        }
+                    }
+                }
+                else
+                {
+                    Final = Read(file);
+                }
+
+                if (!Directory.Exists(Path.GetDirectoryName($"{outPath}\\rom_switch\\{file.Substring(file.LastIndexOf("rom"))}")))
+                    Directory.CreateDirectory(Path.GetDirectoryName($"{outPath}\\rom_switch\\{file.Substring(file.LastIndexOf("rom"))}"));
+                Final.Save($"{outPath}\\rom_switch\\{file.Substring(file.LastIndexOf("rom"))}");
             }
         }
     }
