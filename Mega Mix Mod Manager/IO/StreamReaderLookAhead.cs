5ddb463ce94ac6314d13a52daac43ebc50e71d8a
Moonling <52844176+VelouriasMoon@users.noreply.github.com>
2022-01-22 04:40:37 -0400

    Made StreamReaderLookAhead an extension method

--- Mega Mix Mod Manager/IO/StreamReaderLookAhead.cs
+++ Mega Mix Mod Manager/IO/StreamReaderLookAhead.cs
@@ -9,7 +9,15 @@ namespace Mega_Mix_Mod_Manager.IO
 {
     public static class StreamReaderLookAhead
     {
-        public static string LookAheadLine(StreamReader sr)
+        /// <summary>
+        /// Reads a line of characters from the current stream and returns the data as a string, then returns to the starting point of that line of characters
+        /// </summary>
+        /// <returns>
+        /// The next line from the input stream, or null if the end of the input stream is reached.
+        /// </returns>
+        /// <exception cref="OutOfMemoryException">There is insufficient memory to allocate a buffer for the returned string.</exception>
+        /// <exception cref="IOException">An I/O error occurs.</exception>
+        public static string LookAheadLine(this StreamReader sr)
         {
             long pos = sr.BaseStream.Position;
             string line = sr.ReadLine();
